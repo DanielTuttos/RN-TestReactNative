093a0ee32e78291efa83c6cc616e0b8d3832d573
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-05-05 00:13:18 -0500

    :construction: Favorites Screen finished

--- src/presentation/screens/FavoritesScreen.tsx
+++ src/presentation/screens/FavoritesScreen.tsx
@@ -1,9 +1,42 @@
-import {Text, View} from 'react-native';
+import {Pressable, View} from 'react-native';
+import {MainScreen, TrackCard} from '../components';
+import {FlatList} from 'react-native-gesture-handler';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../navigator/StackNavigator';
+import {useTracksStore} from '../store/tracksStore';
 
-export const FavoritesScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'FavoritesScreen'> {}
+
+export const FavoritesScreen = ({navigation}: Props) => {
+  const {addFavorites, favorites = []} = useTracksStore();
+
   return (
-    <View>
-      <Text>FavoritesScreen</Text>
-    </View>
+    <MainScreen
+      withScroll={false}
+      textHeader="Favorites"
+      header="withBack"
+      onPressBack={() => navigation.goBack()}>
+      <FlatList
+        numColumns={2}
+        data={favorites}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <Pressable
+            onPress={() =>
+              navigation.navigate('DetailScreen', {idTrack: item.id})
+            }>
+            <TrackCard
+              track={item}
+              isFavoriteButton
+              onPressFavorite={() => addFavorites(item)}
+              inFavorites={favorites.some(fav => fav.id === item.id)}
+            />
+          </Pressable>
+        )}
+        style={{height: '94%'}}
+        showsVerticalScrollIndicator={false}
+      />
+      <View style={{height: 70}} />
+    </MainScreen>
   );
 };
